5b2204b9a150e8373891c6afacad6c44c2313656
mocsilya <dev@aylis.com>
2021-05-10 12:57:05 +0100

    update

---
+++ assets/js/theme/custom/card-warranty.js
@@ -0,0 +1,23 @@
+/**
+ * Adds swatches to product cards.
+ * templates > components > products > card.html
+*/
+export default function () {
+	$('.figure__warranty').each(function() {
+		const url = $(this).attr('href');
+		const warranty = $(this).parent().next('.card-body').find('.card-warranty');
+		$.ajax({ 
+			url: url, 
+			processData : false,
+			cache: true,
+			success: function(data) {
+				const html = $.parseHTML( data );
+				const content = $(html).find('.warranty-content').contents();
+				if ($(content).length > 0) { 
+				    $(warranty).find('.dropdown-menu').append(content);
+					$(warranty).show();
+				}
+			}
+		});		
+	});
+}
