94073ed2c62fe07586f97aa1b861023ae8f31c28
mocsilya <dev@aylis.com>
2021-05-16 19:09:38 +0100

    update

--- assets/js/theme/home.js
+++ assets/js/theme/home.js
@@ -3,11 +3,15 @@
  */
 import PageManager from './page-manager';
 import cardSwatches from './custom/card-swatches';
+import cardWarranty from './custom/card-warranty';
+import cardCarousel from './custom/card-carousel';
 import modalSubscribe from './custom/modal-subscribe';
 
 export default class Home extends PageManager {
 	onReady() {
         cardSwatches();
+		cardWarranty();
+		cardCarousel();
         modalSubscribe();
     }
 }
