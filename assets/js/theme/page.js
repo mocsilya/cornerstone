c68b3c4aa0fc813572bd6a54e1ee970de517a6dd
mocsilya <dev@aylis.com>
2021-05-18 17:28:29 +0100

    update

--- assets/js/theme/page.js
+++ assets/js/theme/page.js
@@ -3,9 +3,11 @@
  */
 import PageManager from './page-manager';
 import menuHelper from './custom/menu-helper';
+import tabHash from './custom/tab-hash';
 
 export default class Page extends PageManager {
 	onReady() {
         menuHelper();
+		tabHash();
     }
 }
