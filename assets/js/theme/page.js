69f4365462767b7d47007a365ca86acfc7e6fcd2
mocsilya <dev@aylis.com>
2021-05-19 11:50:12 +0100

    add tab hash

--- assets/js/theme/page.js
+++ assets/js/theme/page.js
@@ -1,11 +1,13 @@
 /*
- Import all homepage specific js
+ Import all page specific js
  */
 import PageManager from './page-manager';
 import menuHelper from './custom/menu-helper';
+import tabHash from './custom/tab-hash';
 
 export default class Page extends PageManager {
 	onReady() {
         menuHelper();
+        tabHash();
     }
 }
