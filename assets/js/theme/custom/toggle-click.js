709f61e9dcf0d52920a838e0ba7006d84cb5f617
mocsilya <dev@aylis.com>
2021-10-24 17:17:14 +0100

    update

---
+++ assets/js/theme/custom/toggle-click.js
@@ -0,0 +1,10 @@
+/**
+ * Open/Close an element with custom toggle link
+*/
+export default function () { 
+	$('.toggle-click').click(function(e) {
+		e.preventDefault();
+		const clickHref = $(this).attr("href");
+		$(clickHref).toggleClass('is-open');
+	});
+}
