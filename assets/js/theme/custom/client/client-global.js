56c5d8ec611d77cecdac642857f99bff915dee79
mocsilya <dev@aylis.com>
2021-11-15 16:07:01 +0000

    update

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -2,5 +2,24 @@
  * Custom global client js
 */
 export default function () { 
-
+	$('.form-option-wrapper-250g .form-option').click(function() {
+		$('.form-field-gift-message').removeClass('is-active');
+		$('.form-field-gift-message .form-input').val('');
+	});
+	$('.form-option-wrapper-500g .form-option').click(function() {
+		$('.form-field-gift-message').removeClass('is-active');
+		$('.form-field-gift-message .form-input').val('');
+	});
+	$('.form-option-wrapper-1kg .form-option').click(function() {
+		$('.form-field-gift-message').removeClass('is-active');
+		$('.form-field-gift-message .form-input').val('');
+	});
+	$('.form-option-wrapper-3kg .form-option').click(function() {
+		$('.form-field-gift-message').removeClass('is-active');
+		$('.form-field-gift-message .form-input').val('');
+	});
+	$('.form-option-wrapper-filled-gift-cube-10cm .form-option').click(function() {
+		$('.form-field-gift-message').addClass('is-active');
+	});
+	
 }
