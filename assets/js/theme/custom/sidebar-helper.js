6f07ac13a67ae5d966014feb32393b5a121ba112
mocsilya <dev@aylis.com>
2021-12-07 13:49:34 +0000

    update

--- assets/js/theme/custom/sidebar-helper.js
+++ assets/js/theme/custom/sidebar-helper.js
@@ -2,7 +2,12 @@
  * Remove sidebar if not in use
 */
 export default function () { 
-	if ($('.page-sidebar').height() == 0) {
-		$('.page-sidebar').remove();
+	if ($('.page-sidebar').length) {
+		if ($('.page-sidebar').height() == 0) {
+			$('.page-sidebar').remove();
+			$('.page-sidebar-toggle').remove();
+		}
+	} else {
+		$('.page-sidebar-toggle').remove();
 	}
 }
