369a0b513fa7a20e38abd80df8b3ae3a4eabc4bd
mocsilya <dev@aylis.com>
2022-11-25 17:16:54 +0000

    update

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -26,6 +26,32 @@ export default function () {
 		}
 	});
 	
+	$('.form-field-jar-size .form-option').click(function() {		
+		if ($(this).hasClass('form-option-small-jar-11cm') || $(this).hasClass('form-option-large-jar-19cm')) {
+			$('.form-field-label-wording').addClass('is-active');
+			$('.form-field-select-label-design').addClass('is-active');
+			if ($('.form-field-label-wording .form-radio-none').is(':checked')) {
+				$('.form-field-label-wording .form-radio-none').prop('checked', false);
+				$('.form-radio-use-default-text').prop('checked', true);
+			}
+		} else {
+			$('.form-field-label-wording').removeClass('is-active');
+			$('.form-radio-type-my-own-text').prop('checked', false);
+			$('.form-radio-use-default-text').prop('checked', false);
+			$('.form-field-label-wording .form-radio-none').prop('checked', true);
+			$('.form-field-custom-text').removeClass('is-active');
+			$('.form-field-custom-text .form-input').val('');
+			$('.form-field-select-label-design').removeClass('is-active');
+			$('.form-field-select-label-design .form-select').prop('selectedIndex',0);
+		}
+	});
+	
+	$('.form-select-select-label-design').on('change', function() {
+		$('.form-field-label-wording').addClass('is-active');
+		$('.form-radio-use-default-text').prop('checked', true);
+		$('.form-field-custom-text .form-input').val('');
+	});
+	
 	$('.form-field-label-wording .form-radio').click(function() {		
 		if ($(this).hasClass("form-radio-type-my-own-text")) {
 			$('.form-field-custom-text').addClass('is-active');
