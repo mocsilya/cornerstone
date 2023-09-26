65155af0cae5accd658505e331137b69f4058c98
mocsilya <dev@aylis.com>
2023-09-26 16:15:27 +0100

    update

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -1,6 +1,40 @@
 /**
  * Custom global client js
 */
-export default function () { 
+import 'slick-carousel';
 
+export default function loaded () {
+	$('.review-slider').slick({
+		slidesToShow: 1,
+        slidesToScroll: 1,
+		swipe: true,
+		draggable: true,
+		touchMove: true,
+        infinite: false,
+		autoplay: false,
+		autoplaySpeed: 6000,
+        dots: false,
+		arrows: true,
+		mobileFirst: true,
+        responsive: [
+			{
+				breakpoint: 801,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 1260,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 1439,
+				settings: {
+					slidesToShow: 4
+				}
+			}
+		]
+	});
 }
