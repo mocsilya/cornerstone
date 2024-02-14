764afff7f8096f824f98065b1ee679551a39bd76
mocsilya <dev@aylis.com>
2024-02-14 18:00:49 +0000

    sticky add to cart

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -6,4 +6,37 @@ export default function () {
 		e.preventDefault();
 		$(".cart-terms").hide();
 	});
+	
+	if ($(window).width() > 1260) {	
+		
+		var totalWidth = $('.productView').outerWidth();
+		var halfWidth = totalWidth / 2;
+		var photoWidth = $('.productView-images.productView-images-large').outerWidth();
+		var dataWidth = $('.productView-details-large.product-data').outerWidth();
+		
+		var leftMargin = photoWidth - halfWidth;
+		
+		$('.productView-details-large.product-data, .productView-details-large.product-options').css('width', dataWidth);
+		
+		$(window).scroll(function() {    
+		    var scroll = $(window).scrollTop();
+		    if (scroll > 168) {
+		        $('.productView-details-large.product-data, .productView-details-large.product-options').addClass('productView-details-fixed').css('margin-left', leftMargin);
+		    } else {
+		        $('.productView-details-large.product-data, .productView-details-large.product-options').removeClass('productView-details-fixed').css('margin-left', '0px');
+		    }
+			
+			var top_of_element = $(".related-similar").offset().top;
+			var bottom_of_element = $(".related-similar").offset().top + $(".related-similar").outerHeight();
+			var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+			var top_of_screen = $(window).scrollTop();
+			
+			//if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+			if (bottom_of_screen > top_of_element) {
+				$('.productView-details-large.product-data, .productView-details-large.product-options').removeClass('productView-details-fixed').css('margin-left', '0px');
+			} else {
+				// the element is not visible, do something else
+			}
+		});
+	}
 }
