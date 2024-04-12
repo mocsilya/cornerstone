596ad244a12d43ad3556282503044f90ec9d9a7f
mocsilya <dev@aylis.com>
2024-04-12 12:28:14 +0100

    flyout

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -1,6 +1,26 @@
 /**
  * Custom global client js
 */
-export default function () { 
 
+export default function () {
+	if (localStorage.getItem('flyoutBanner') == '#flyoutShow') {
+	    $('#flyoutBanner').addClass('is-open');
+	}
+	if (localStorage.getItem('flyoutBanner') == '#flyoutHide') {
+	    $('#flyoutBanner').removeClass('is-open');
+	}
+	
+	$('.flyout-click').click(function(e) {
+		e.preventDefault();
+		const clickHref = $(this).attr('href');
+		if ($('#flyoutBanner').hasClass('is-open')) {
+			$('#flyoutBanner').removeClass('is-open');
+			localStorage.removeItem('flyoutBanner');
+			localStorage.setItem('flyoutBanner', clickHref);
+		} else {
+			$('#flyoutBanner').addClass('is-open');
+			localStorage.removeItem('flyoutBanner');
+			localStorage.setItem('flyoutBanner', clickHref);
+		}
+	});
 }
