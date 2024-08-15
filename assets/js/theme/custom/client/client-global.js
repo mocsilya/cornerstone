e5b40deeb6936759fbf8c7e67ffbcbbbf5aba99f
mocsilya <dev@aylis.com>
2024-08-15 10:26:24 +0100

    update

--- assets/js/theme/custom/client/client-global.js
+++ assets/js/theme/custom/client/client-global.js
@@ -5,7 +5,11 @@ export default function () {
 	$('.navPages-item-e-liquids .navPage-subMenu-item').hover(function() {     
 		$('.navPages-item-e-liquids .navPage-childList').removeClass('sfHover');
 		$('.navPages-item-e-liquids .navPage-subMenu-item').removeClass('sfHover');
+		$(this).closest('div').removeClass('flavourHover');
 		$(this).addClass('sfHover');
 		$(this).children('.navPage-childList').addClass('sfHover');
+		if ($(this).hasClass('navPage-subMenu-flavours')) {
+			$(this).closest('div').addClass('flavourHover');
+		}
 	});
 }
